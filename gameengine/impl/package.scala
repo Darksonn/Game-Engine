06cd59e29439df7a20b9689b7762f66e744d8cd9
tailcalled <tailcalled@gmail.com>
2013-06-17 21:42:32 +0200

    uhh

--- gameengine/impl/package.scala
+++ gameengine/impl/package.scala
@@ -1,13 +1,39 @@
 package gameengine
 
 import javax.swing._
-import java.awt.{Color, Graphics, Graphics2D}
+import java.awt.{Color, Graphics, Graphics2D, Dimension}
 
 package object impl {
 
+	val todo = null
+	val fps = 60
+	val nspf = 1000000000L / fps
+
+	class GameComponent(game: Game) extends JComponent {
+		setPreferredSize(new Dimension(game.width, game.height))
+	}
+
 	def run(game: gameengine.Game): Unit = {
 		val window = new JFrame(game.title)
+		val comp = new GameComponent(game)
+		window.add(comp)
+		window.pack()
 		window.setLocationRelativeTo(null)
+		window.setVisible(true)
+		window.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE)
+		var running = true
+		var lastTime = System.nanoTime()
+		while (running) {
+			game.step(todo) match {
+				case Some(ControlUpdate.Quit) =>
+					running = false
+			}
+			if (running) {
+				game.render(todo)
+			}
+			val nowTime = System.nanoTime()
+			Thread.sleep(((nspf - (nowTime - lastTime)) / 1000000L) max 1L)
+		}
 	}
 
 }
