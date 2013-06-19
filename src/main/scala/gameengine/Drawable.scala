46ba1d3651f3c3d421bb871fcbb55ac42cbbe54c
Darksonn <kristoffer@ryhl.dk>
2013-06-19 16:44:30 +0200

    Created load image functions in Drawable.scala

--- src/main/scala/gameengine/Drawable.scala
+++ src/main/scala/gameengine/Drawable.scala
@@ -9,6 +9,8 @@ object Drawable {
 	def loadImage(image: java.awt.Image): Drawable = new Drawable {
 		def draw(gfx: Graphics2D): Unit = gfx.drawImage(image, 0, 0, null)
 	}
+	def loadImage(path: String): Drawable = loadImage(classOf[Drawable].getResource(if (path.startsWith("/")) path else "/" + path))
+	def loadImage(url: java.net.URL): Drawable = loadImage(javax.imageio.ImageIO.read(url))
 }
 class DrawableText(text: String, font: java.awt.Font, color: java.awt.Color) extends Drawable {
 	protected[gameengine] def draw(gfx: Graphics2D): Unit = {
