7c84ad76949a3464b04a50797eb13f6686d26d3c
Darksonn <kristoffer@ryhl.dk>
2013-06-19 16:15:09 +0200

    Fixed missing import

--- src/main/scala/gameengine/Drawable.scala
+++ src/main/scala/gameengine/Drawable.scala
@@ -1,13 +1,17 @@
 package gameengine
 
+import java.awt.Graphics2D
+
 trait Drawable {
-	def draw(gfx: Graphics2D): Unit
+	protected def draw(gfx: Graphics2D): Unit
 }
-class DrawableImage(image: java.awt.Image) extends Drawable {
-	def draw(gfx: Graphics2D): Unit = gfx.drawImage(image)
+object Drawable {
+	def loadImage(image: java.awt.Image): Drawable = new Drawable {
+		def draw(gfx: Graphics2D): Unit = gfx.drawImage(image)
+	}
 }
 class DrawableText(text: String, font: java.awt.Font, color: java.awt.Color) extends Drawable {
-	def draw(gfx: Graphics2D): Unit = {
+	protected def draw(gfx: Graphics2D): Unit = {
 		gfx.setFont(font)
 		gfx.setColor(color)
 		gfx.drawString(text, 0, 0)
