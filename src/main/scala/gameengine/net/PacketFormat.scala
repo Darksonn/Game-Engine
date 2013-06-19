7992704ff84401125ef10e6fc44d82477b7b58b5
Darksonn <kristoffer@ryhl.dk>
2013-06-19 19:45:37 +0200

    Added packetformats for some primitive types

--- src/main/scala/gameengine/net/PacketFormat.scala
+++ src/main/scala/gameengine/net/PacketFormat.scala
@@ -1,5 +1,8 @@
 package gameengine.net
 
+import scala.collection.immutable.List
+import java.io.IOException
+
 trait PacketFormat[T] {
 	def send(obj: T, c: Connection): Unit
 	def receive(c: Connection): T
@@ -28,7 +31,49 @@ object StringPacketFormat extends PacketFormat[String] {
 	def receive(c: Connection): String = {
 		val id: Byte = c.in.readByte()
 		if (id != 20)
-			throw new java.io.IOException("Recieved unexpected data type.")
+			throw new IOException("Recieved unexpected data type.")
 		c.in.readUTF()
 	}
+}
+object BytePacketFormat extends PacketFormat[Byte] {
+	def send(b: Byte, c: Connection): Unit = {
+		c.out.writeByte(b)
+		c.flush()
+	}
+	def receive(c: Connection): Byte = c.in.readByte()
+}
+object ShortPacketFormat extends PacketFormat[Short] {
+	def send(short: Short, c: Connection): Unit = {
+		c.out.writeShort(short)
+		c.flush()
+	}
+	def receive(c: Connection): Short = c.in.readShort()
+}
+object IntPacketFormat extends PacketFormat[Int] {
+	def send(i: Int, c: Connection): Unit = {
+		c.out.writeInt(i)
+		c.flush()
+	}
+	def receive(c: Connection): Int = c.in.readInt()
+}
+object FloatPacketFormat extends PacketFormat[Float] {
+	def send(i: Float, c: Connection): Unit = {
+		c.out.writeFloat(i)
+		c.flush()
+	}
+	def receive(c: Connection): Float = c.in.readFloat()
+}
+object DoublePacketFormat extends PacketFormat[Double] {
+	def send(i: Double, c: Connection): Unit = {
+		c.out.writeDouble(i)
+		c.flush()
+	}
+	def receive(c: Connection): Double = c.in.readDouble()
+}
+object BooleanPacketFormat extends PacketFormat[Boolean] {
+	def send(i: Boolean, c: Connection): Unit = {
+		c.out.writeBoolean(i)
+		c.flush()
+	}
+	def receive(c: Connection): Boolean = c.in.readBoolean()
 }
