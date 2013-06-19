cdd049654d5c1ae9696a45bf137e15c2235e6ef9
tailcalled <tailcalled@gmail.com>
2013-06-19 16:20:18 +0200

    .

--- src/main/scala/gameengine/Drawable.scala
+++ src/main/scala/gameengine/Drawable.scala
@@ -3,15 +3,15 @@ package gameengine
 import java.awt.Graphics2D
 
 trait Drawable {
-	protected def draw(gfx: Graphics2D): Unit
+	protected[gameengine] def draw(gfx: Graphics2D): Unit
 }
 object Drawable {
 	def loadImage(image: java.awt.Image): Drawable = new Drawable {
-		def draw(gfx: Graphics2D): Unit = gfx.drawImage(image)
+		def draw(gfx: Graphics2D): Unit = gfx.drawImage(image, 0, 0, null)
 	}
 }
 class DrawableText(text: String, font: java.awt.Font, color: java.awt.Color) extends Drawable {
-	protected def draw(gfx: Graphics2D): Unit = {
+	protected[gameengine] def draw(gfx: Graphics2D): Unit = {
 		gfx.setFont(font)
 		gfx.setColor(color)
 		gfx.drawString(text, 0, 0)
