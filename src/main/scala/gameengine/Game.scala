160b5cce31c43c4ce7a35d67279e1ba6b9514ed7
Nullable <teo@nullable.se>
2013-06-19 17:25:59 +0200

    Indent with tabs

--- src/main/scala/gameengine/Game.scala
+++ src/main/scala/gameengine/Game.scala
@@ -4,17 +4,17 @@ import java.awt.Color
 import java.awt.event.{MouseEvent => AWTMouseEvent}
 
 trait Game {
-	
-	val width: Int
-	val height: Int
-	val title: String
 
-	def step(in: Input): Option[ControlUpdate]
-	def render(gfx: Output): Unit
+  val width: Int
+  val height: Int
+  val title: String
 
-	def main(args: Array[String]): Unit = {
-		impl.run(this)
-	}
+  def step(in: Input): Option[ControlUpdate]
+  def render(gfx: Output): Unit
+
+  def main(args: Array[String]): Unit = {
+    impl.run(this)
+  }
 
 }
 
@@ -25,12 +25,12 @@ object Key {
   val RightMouse = MouseButton(AWTMouseEvent.BUTTON2)
 
   case class MouseButton(code: Int) extends Key
-	case class KeyboardKey(code: Int) extends Key
+  case class KeyboardKey(code: Int) extends Key
 }
 
 sealed trait ControlUpdate
 object ControlUpdate {
-	object Quit extends ControlUpdate
+  object Quit extends ControlUpdate
 }
 
 case class Input(queue: Seq[InputEvent])
@@ -46,16 +46,16 @@ case class MouseClickEvent(key: Key, pos: Point) extends MouseInputEvent
 case class MouseMoveEvent(to: Point) extends MouseInputEvent
 
 trait Output {
-	def withRotation(radians: Double)(body: => Unit): Unit
-	def withScaling(scaleX: Double, scaleY: Double)(body: => Unit): Unit
-	def withTranslation(deltaX: Double, deltaY: Double)(body: => Unit): Unit
-	def drawFilledCircle(c: Color): Unit
-	def drawFilledRect(c: Color): Unit
-	def drawCircle(c: Color): Unit
-	def drawRect(c: Color): Unit
-	def drawFilledOval(x: Double, y: Double, w: Double, h: Double, c: Color): Unit
-	def drawFilledRect(x: Double, y: Double, w: Double, h: Double, c: Color): Unit
-	def drawOval(x: Double, y: Double, w: Double, h: Double, c: Color): Unit
-	def drawRect(x: Double, y: Double, w: Double, h: Double, c: Color): Unit
-	def draw(drawable: Drawable): Unit
+  def withRotation(radians: Double)(body: => Unit): Unit
+  def withScaling(scaleX: Double, scaleY: Double)(body: => Unit): Unit
+  def withTranslation(deltaX: Double, deltaY: Double)(body: => Unit): Unit
+  def drawFilledCircle(c: Color): Unit
+  def drawFilledRect(c: Color): Unit
+  def drawCircle(c: Color): Unit
+  def drawRect(c: Color): Unit
+  def drawFilledOval(x: Double, y: Double, w: Double, h: Double, c: Color): Unit
+  def drawFilledRect(x: Double, y: Double, w: Double, h: Double, c: Color): Unit
+  def drawOval(x: Double, y: Double, w: Double, h: Double, c: Color): Unit
+  def drawRect(x: Double, y: Double, w: Double, h: Double, c: Color): Unit
+  def draw(drawable: Drawable): Unit
 }
