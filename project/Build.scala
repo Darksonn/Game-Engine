f733d357ff33cfe27bb5125ea440e003046b48c7
Nullable <teo@nullable.se>
2013-06-24 17:57:12 +0200

    Renamed game-engine to game-engine-core, split out the demos and net to their own projects

--- project/Build.scala
+++ project/Build.scala
@@ -12,8 +12,10 @@ object GameEngineBuild extends Build {
 		scalaVersion := Deps.V.Scala
 	)
 
-	lazy val root = Project("game-engine", file("."), settings = Project.defaultSettings) aggregate (gameEngine, functional)
+	lazy val root = Project("game-engine", file("."), settings = Project.defaultSettings) aggregate (core, functional, demos)
 
-	lazy val gameEngine = Project("game-engine-core", file("game-engine"), settings = Project.defaultSettings)
-	lazy val functional = Project("game-engine-frp", file("game-engine-frp"), settings = Project.defaultSettings) dependsOn gameEngine
+	lazy val core = Project("game-engine-core", file("game-engine-core"), settings = Project.defaultSettings)
+	lazy val functional = Project("game-engine-frp", file("game-engine-frp"), settings = Project.defaultSettings) dependsOn core
+	lazy val demos = Project("game-engine-demos", file("game-engine-demos"), settings = Project.defaultSettings) dependsOn core
+	lazy val net = Project("game-engine-net", file("game-engine-net"), settings = Project.defaultSettings) dependsOn core
 }
