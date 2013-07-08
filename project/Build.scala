510d3dcba4579f7af536bb00180228efdea29027
tailcalled <tailcalled@gmail.com>
2013-07-08 12:17:31 +0200

    Added new project for an LWJGL implementation.

--- project/Build.scala
+++ project/Build.scala
@@ -9,9 +9,11 @@ object GameEngineBuild extends Build {
 			val Scala = "2.10.2"
 			val Scalatest = "2.0.M5b"
 			val Scalamock = "3.0.1"
+			val LWJGL = "2.9.0"
 		}
 		val Scalatest = "org.scalatest" %% "scalatest" % V.Scalatest % "test"
 		val Scalamock = "org.scalamock" %% "scalamock-scalatest-support" % V.Scalamock % "test"
+		val LWJGL = "org.lwjgl.lwjgl" % "lwjgl" % V.LWJGL
 	}
 
 	override lazy val settings = super.settings ++ Seq(
@@ -29,11 +31,12 @@ object GameEngineBuild extends Build {
 	lazy val root = Project("game-engine", file("."), settings = masterSettings ++ Seq(
 		run in Runtime <<= run in (demos, Runtime),
 		run in Compile <<= run in Runtime
-	)) aggregate (core, functional, demos, net, states)
+	)) aggregate (core, functional, demos, net, states, lwjgl)
 
 	lazy val core = Project("game-engine-core", file("game-engine-core"), settings = Project.defaultSettings)
 	lazy val functional = Project("game-engine-frp", file("game-engine-frp"), settings = Project.defaultSettings) dependsOn core
 	lazy val demos = Project("game-engine-demos", file("game-engine-demos"), settings = Project.defaultSettings) dependsOn(core, net, functional)
 	lazy val net = Project("game-engine-net", file("game-engine-net"), settings = Project.defaultSettings) dependsOn core
 	lazy val states = Project("game-engine-states", file("game-engine-states"), settings = Project.defaultSettings) dependsOn core
+	lazy val lwjgl = Project("game-engine-lwjgl", file("game-engine-lwjgl"), settings = Project.defaultSettings) settings (libraryDependencies += Deps.LWJGL) dependsOn core
 }
