b8121374fa6ba5a74dc78a7390bad468741a0871
Darksonn <kristoffer@ryhl.dk>
2013-07-13 20:16:21 +0200

    Added point to vector and vector to point functions

--- game-engine-core/src/main/scala/gameengine/Geom.scala
+++ game-engine-core/src/main/scala/gameengine/Geom.scala
@@ -1,11 +1,14 @@
 package gameengine
 
-case class Point(x: Int, y: Int)
+case class Point(x: Int, y: Int) {
+	def toVector = Vector2D(this)
+}
 /**
  * Can function like a normal FloatingPoint but has some manipulation functions
  */ 
 final class Vector2D(val x: Double, val y: Double) {
 	def length = Math.sqrt(x * x + y * y)
+	def lengthSquared = length*length
 	def angle = Math.atan2(y,x)
 	def +(other: Vector2D) = Vector2D(x + other.x, y + other.y)
 	def -(other: Vector2D) = Vector2D(x - other.x, y - other.y)
@@ -14,8 +17,10 @@ final class Vector2D(val x: Double, val y: Double) {
 	def *(factor: Double) = Vector2D(x * factor, y * factor)
 	def normalize = Vector2D(Math.cos(angle), Math.sin(angle))
 	def transform(center: Vector2D) = Vector2D(x - center.x, y - center.y)
+	def toPoint = Point(x.intValue, y.intValue)
 }
 object Vector2D {
+	def apply(pos: Point) = new Vector2D(pos.x, pos.y)
 	def apply(x: Double, y: Double) = new Vector2D(x, y)
-	def polar(angle: Double, length: Double) = Vector2D(Math.cos(angle), Math.sin(angle)) * length
+	def polar(angle: Double, length: Double) = new Vector2D(Math.cos(angle), Math.sin(angle)) * length
 }
