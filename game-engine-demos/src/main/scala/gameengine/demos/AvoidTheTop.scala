877c6d41d281df71116409f9855c282b8a3cd4ef
Darksonn <kristoffer@ryhl.dk>
2013-07-16 00:25:45 +0200

    Fixed a bug

--- game-engine-demos/src/main/scala/gameengine/demos/AvoidTheTop.scala
+++ game-engine-demos/src/main/scala/gameengine/demos/AvoidTheTop.scala
@@ -2,8 +2,11 @@ package gameengine.demos
 
 import gameengine._
 import gameengine.styles.{ImperativeControlStyle, Game, EventInputStyle}
+import java.io.File
 
-object AvoidTheTop extends Game with EventInputStyle with ImperativeControlStyle {
+object AvoidTheTopGame extends Game with EventInputStyle with ImperativeControlStyle {
+
+	val datafile = new File(new File(System.getProperty("user.home", ".")), "avoidthetop.data")
 
 	val width = 640
 	val height = 480
@@ -16,55 +19,95 @@ object AvoidTheTop extends Game with EventInputStyle with ImperativeControlStyle
 	var leftPressed = false
 	var paused = false
 	var count = 0
+	var startTime: Long = -1
+	var gamestate = -1
+	var maxScore: Long = 0
+	var lastScore: Long = 0
 
 	override def update(in: Input) = {
-		for (event <- in.queue) {
-			event match {
-				case KeyDownEvent(key) =>
-					key match {
-						case Key.KeyboardKey(code) =>
-							code match {
-								case 37 => leftPressed = true
-								case 39 => rightPressed = true
-								case 80 => paused = !paused
+		gamestate match {
+			case -1 =>
+				if (datafile.exists) {
+					val in = new java.io.DataInputStream(new java.io.FileInputStream(datafile))
+					maxScore = in.readLong
+					lastScore = in.readLong
+					in.close
+				}
+				gamestate = 0
+			case 0 =>
+				for (event <- in.queue) {
+					event match {
+						case KeyDownEvent(key) =>
+							key match {
+								case Key.LeftMouse =>
+									gamestate = 1
 								case _ => Unit
 							}
 						case _ => Unit
 					}
-				case KeyUpEvent(key) =>
-					key match {
-						case Key.KeyboardKey(code) =>
-							code match {
-								case 37 => leftPressed = false
-								case 39 => rightPressed = false
+				}
+			case 1 => 
+				if (startTime == -1) {
+					startTime = System.currentTimeMillis
+				}
+				for (event <- in.queue) {
+					event match {
+						case KeyDownEvent(key) =>
+							key match {
+								case Key.KeyboardKey(code) =>
+									code match {
+										case 37 => leftPressed = true
+										case 39 => rightPressed = true
+										case 80 => paused = !paused
+										case _ => Unit
+									}
+								case _ => Unit
+							}
+						case KeyUpEvent(key) =>
+							key match {
+								case Key.KeyboardKey(code) =>
+									code match {
+										case 37 => leftPressed = false
+										case 39 => rightPressed = false
+										case _ => Unit
+									}
 								case _ => Unit
 							}
 						case _ => Unit
 					}
-				case _ => Unit
-			}
-		}
-		if (!paused) {
-			if (rightPressed) player = player.changeX(5)
-			if (leftPressed) player = player.changeX(-5)
+				}
+				if (!paused) {
+					if (rightPressed) player = player.changeX(5)
+					if (leftPressed) player = player.changeX(-5)
 
-			if (player.y < height-20) player = player.changeY(2)
-			var newPlatforms = platforms
-			for (p <- platforms) {
-				if (p.removeMe) newPlatforms = newPlatforms diff Seq(p)
-				if (p.touches(player)) player = player.changeY(-3)
-				p.update
-			}
-			platforms = newPlatforms
-			if (count % (30 * (1.0 / Platform.platformSpeed)) == 0) {
-				platforms = platforms :+ Platform(r.nextInt(width-Platform.holeWidth-10), height)
-			}
-			count += 1
+					if (player.y < height-20) player = player.changeY(2)
+					var newPlatforms = platforms
+					for (p <- platforms) {
+						if (p.removeMe) newPlatforms = newPlatforms diff Seq(p)
+						if (p.touches(player)) player = player.changeY(-3)
+						p.update
+					}
+					platforms = newPlatforms
+					if (count % (30 * (1.0 / Platform.platformSpeed)) == 0) {
+						platforms = platforms :+ Platform(r.nextInt(width-Platform.holeWidth-10), height)
+					}
+					count += 1
+					if (player.y < 0) {
+						val score = System.currentTimeMillis - startTime
+						startTime = -1
+						gamestate = -1
+						rightPressed = false
+						leftPressed = false
+						platforms = Seq[Platform]()
+						player = Vector2D(width/2, 20)
+						count = 0
 
-			if (player.y < 0) {
-				quit()
-				println("Game over")
-			}
+						val out = new java.io.DataOutputStream(new java.io.FileOutputStream(datafile))
+						out.writeLong(if (maxScore > score) {maxScore} else {score})
+						out.writeLong(score)
+						out.close
+					}
+				}
 		}
 		Seq()
 	}
@@ -73,11 +116,31 @@ object AvoidTheTop extends Game with EventInputStyle with ImperativeControlStyle
 		withScaling(width, height) {
 			drawFilledRect(java.awt.Color.BLACK)
 		}
-		for (p <- platforms) {
-			drawFilledRect(0, p.y, p.hole, Platform.platformHeight, java.awt.Color.WHITE)
-			drawFilledRect(p.hole+Platform.holeWidth, p.y, width-p.hole+Platform.holeWidth, Platform.platformHeight, java.awt.Color.WHITE)
+		gamestate match {
+			case 0 =>
+				withTranslation(10,20) {
+					withScaling(20,20) {
+						draw(new DrawableText("Best score: " + (maxScore/1000.0) + " seconds", new java.awt.Font("Monospaced", 12, 1), java.awt.Color.WHITE))
+					}
+				}
+				withTranslation(10,80) {
+					withScaling(20,20) {
+						draw(new DrawableText("Click anywhere to play", new java.awt.Font("Monospaced", 12, 1), java.awt.Color.WHITE))
+					}
+				}
+				withTranslation(10,50) {
+					withScaling(20,20) {
+						draw(new DrawableText("Last score: " + (lastScore/1000.0) + " seconds", new java.awt.Font("Monospaced", 12, 1), java.awt.Color.WHITE))
+					}
+				}
+			case 1 =>
+				for (p <- platforms) {
+					drawFilledRect(0, p.y, p.hole, Platform.platformHeight, java.awt.Color.WHITE)
+					drawFilledRect(p.hole+Platform.holeWidth, p.y, width-p.hole+Platform.holeWidth, Platform.platformHeight, java.awt.Color.WHITE)
+				}
+				drawFilledRect(player.x, player.y-4, 3, 3, java.awt.Color.RED)
+			case _ => Unit
 		}
-		drawFilledRect(player.x, player.y-4, 3, 3, java.awt.Color.RED)
 	}
 
 	override val on: PartialFunction[InputEvent, Unit] = {
