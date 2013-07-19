17ac0a0c67171e8135320cd2781a93d1ff36efb4
Darksonn <kristoffer@ryhl.dk>
2013-07-19 22:02:22 +0200

    Now lwjgl only stores one copy of libraries in the temp folder

--- game-engine-lwjgl/src/main/scala/gameengine/lwjgl/LWJGLGame.scala
+++ game-engine-lwjgl/src/main/scala/gameengine/lwjgl/LWJGLGame.scala
@@ -4,7 +4,9 @@ import gameengine.BaseGame
 import java.io._
 import java.util.zip.{ZipEntry, ZipInputStream}
 
-trait LWJGLGame extends BaseGame {
+object LWJGLLoader {
+
+	var isLoaded = false
 
 	private val loadOrder32win = Seq("OpenAL32.dll", "jinput-raw.dll", "jinput-dx8.dll", "lwjgl.dll")
 	private val loadOrder64win = Seq("OpenAL64.dll", "jinput-raw_64.dll", "jinput-dx8_64.dll", "lwjgl64.dll")
@@ -12,38 +14,34 @@ trait LWJGLGame extends BaseGame {
 	private val loadOrder64linux = Seq("libopenal64.so", "libjinput-linux64.so", "liblwjgl64.so")
 	private val loadOrderosx = Seq("openal.dylib", "libjinput-osx.jnilib", "liblwjgl.jnilib")
 
-	override def main(args: Array[String]) = {
+
+	def load = {
+		if (!isLoaded) {
+			var tmpDirproperty = System.getProperty("java.io.tmpdir")
+			if (tmpDirproperty == null)
+				tmpDirproperty = "."
+			val tempDir = new File(new File(tmpDirproperty), "gameengine-lwjgl-lib-temp")
+			extractLibs(tempDir)
+			System.setProperty("org.lwjgl.librarypath", tempDir.getAbsolutePath)
+			for (load <- getLoadOrder) {
+				System.load(new File(tempDir, load).getAbsolutePath)
+			}
+		}
+	}
+
+	private def extractLibs(tempDir: File) = {
 		javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName())
-		val urls = Thread.currentThread.getContextClassLoader.asInstanceOf[java.net.URLClassLoader].getURLs//.filter(Seq("jinput-dx8_64.dll", "jinput-dx8.dll", "jinput-raw_64.dll", "jinput-raw.dll", "lwjgl.dll", "lwjgl64.dll", "OpenAL32.dll", "OpenAL64.dll") contains _)
-		val temp = File.createTempFile("xxx", "xxx")
-		temp.delete
-		val tempDir = new File(temp.getParentFile, "temp" + randomName)
+		val urls = Thread.currentThread.getContextClassLoader.asInstanceOf[java.net.URLClassLoader].getURLs
 		tempDir.mkdirs
-		var libs = Seq[File]()
+		val loadOrder = getLoadOrder
 		for (url <- urls) {
 			val jar = new File(url.toURI)
 			val jarName = jar.getName
 			val regex = ("(jinput|lwjgl)[-]platform[-][0-9]*(\\.[0-9]*)+[-]natives[-]" + getPlatform + "\\.jar").r
 			val check = regex findFirstIn jarName
 			if (check.nonEmpty)
-				libs = libs ++ extract(jar, tempDir)
+				extract(jar, tempDir, loadOrder)
 		}
-		System.setProperty("org.lwjgl.librarypath", tempDir.getAbsolutePath)
-		/*val sysPathsField = classOf[ClassLoader].getDeclaredField("sys_paths")
-		sysPathsField.setAccessible(true)
-		sysPathsField.set(null, null)*/
-		for (load <- getLoadOrder) {
-			System.load(new File(tempDir, load).getAbsolutePath)
-		}
-		Runtime.getRuntime().addShutdownHook(new Thread {
-			override def run() {
-				for (lib <- libs) {
-					lib.delete
-				}
-				tempDir.delete
-			}
-		})
-		impl.run(this)
 	}
 
 	private def getLoadOrder = {
@@ -60,25 +58,30 @@ trait LWJGLGame extends BaseGame {
 			case _ => loadOrder32linux
 		}
 	}
-	private def randomName(): String = (new java.util.Random).nextLong.toHexString
-	private def extract(jarFile: File, extractLocation: File): Seq[File] = {
+	private def extract(jarFile: File, extractLocation: File, names: Seq[String]): Seq[File] = {
 		var files = Seq[File]()
 		val zinstream = new ZipInputStream(new FileInputStream(jarFile))
 		var zentry = zinstream.getNextEntry
 		var buf = new Array[Byte](1024)
 		while (zentry != null) {
 			val entryName = zentry.getName
-			if (!entryName.contains("META-INF")) {
+			if (names.contains(entryName)) {
 				val entryFile = new File(extractLocation, entryName)
-				files = files :+ entryFile
-				val outstream = new FileOutputStream(entryFile)
-				var n = zinstream.read(buf, 0, 1024)
-				while (n > -1) {
-					outstream.write(buf, 0, n)
-					n = zinstream.read(buf, 0, 1024)
+				if (entryFile.exists) {
+					entryFile.delete
 				}
-				outstream.close
-				zinstream.closeEntry
+				if (!entryFile.exists) {//If delete didn't delete, we assume it is in use
+					entryFile.createNewFile
+					files = files :+ entryFile
+					val outstream = new FileOutputStream(entryFile)
+					var n = zinstream.read(buf, 0, 1024)
+					while (n > -1) {
+						outstream.write(buf, 0, n)
+						n = zinstream.read(buf, 0, 1024)
+					}
+					outstream.close
+					zinstream.closeEntry
+				}
 			}
 			zentry = zinstream.getNextEntry
 		}
@@ -96,5 +99,15 @@ trait LWJGLGame extends BaseGame {
 		if (osName.contains("mac")) return "osx"
 		throw new RuntimeException("Unknown OS")
 	}
+
+}
+
+trait LWJGLGame extends BaseGame {
+
+	override def main(args: Array[String]) = {
+		LWJGLLoader.load
+		impl.run(this)
+	}
+
 	
 }
