ba1d7e533490f79bfaa17fa28faed1687fc16752
Darksonn <kristoffer@ryhl.dk>
2013-08-03 19:59:53 +0200

    Added RSA encryption

--- game-engine-demos/src/main/scala/gameengine/demos/NetTest.scala
+++ game-engine-demos/src/main/scala/gameengine/demos/NetTest.scala
@@ -5,6 +5,8 @@ object NetTest {
 		println("Please enter the part of this test you would like to run:")
 		println("1: Start the server for the Diffie-Hellman Key Exchange test")
 		println("2: Start the client for the Diffie-Hellman Key Exchange test")
+		println("3: RSA encrypt file")
+		println("4: RSA decrypt file")
 		val ln = readLine()
 		ln match {
 			case "1" =>
@@ -16,8 +18,69 @@ object NetTest {
 				println("Connecting to server run by the server function in this program.")
 				val c = Connection("localhost", 2149)
 				println(new BigInt(new java.math.BigInteger(Security.generateKey2(c))))
+			case "3" =>
+				val f = readLine()
+				val from = f + "\\" + "testin.zip"
+				val target = f + "\\" + "test"
+				val keys = readKeypair(f)
+				keys match {
+					case (pubkey, prikey) =>
+						val fromStream = new java.io.FileInputStream(from)
+						val toStream = new Security.RSAOutputStream(new java.io.FileOutputStream(target), pubkey)
+						var c = 0
+						while (fromStream.available > 0) {
+							val bytes = new Array[Byte](512)
+							val len = fromStream.read(bytes)
+							toStream.write(bytes, 0, len)
+							c += 1
+						}
+						fromStream.close
+						toStream.flush
+						toStream.close
+					case _ => throw new Error
+				}
+			case "4" =>
+				val f = readLine()
+				val from = f + "\\" + "test"
+				val target = f + "\\" + "test.zip"
+				val keys = readKeypair(f)
+				keys match {
+					case (pubkey, prikey) =>
+						val fromStream = new Security.RSAInputStream(new java.io.FileInputStream(from), prikey)
+						val toStream = new java.io.FileOutputStream(target)
+						while (fromStream.available > 0) {
+							val bytes = new Array[Byte](512)
+							val len = fromStream.read(bytes)
+							//println(len)
+							toStream.write(bytes, 0, len)
+						}
+						fromStream.close
+						toStream.flush
+						toStream.close
+					case _ => throw new Error
+				}
 			case _ =>
 				println("Illegal input")
+		}
+	}
+	def readKeypair(s: String): (Security.RSAPublicKey, Security.RSAPrivateKey) = {
+		val pub = new java.io.File(new java.io.File(s), "pub.key")
+		val pri = new java.io.File(new java.io.File(s), "pri.key")
+		if (pub.exists) {
+			return (Security.RSAPublicKey.readFrom(new java.io.FileInputStream(pub)), Security.RSAPrivateKey.readFrom(new java.io.FileInputStream(pri)))
+		} else {
+			val keypair = Security.RSA.generateKeypair(1024)
+			keypair match {
+				case (pubkey, prikey) =>
+					val x1 = new java.io.FileOutputStream(pub)
+					val x2 = new java.io.FileOutputStream(pri)
+					pubkey.writeTo(x1)
+					prikey.writeTo(x2)
+					x1.close
+					x2.close
+				case _ => throw new Error
+			}
+			return keypair
 		}
 	}
 }
--- game-engine-net/src/main/scala/gameengine/net/Security.scala
+++ game-engine-net/src/main/scala/gameengine/net/Security.scala
@@ -2,7 +2,10 @@ package gameengine.net
 
 import java.math.BigInteger
 import java.util.Random
-import java.security.SecureRandom
+import java.security._
+import java.io.OutputStream
+import java.io.InputStream
+import java.util.Arrays
 
 object Security {
 	def generatePrime() = new BigInt(BigInteger.probablePrime(512, new Random))
@@ -42,7 +45,7 @@ object Security {
 		return a
 	}
 	def lcm(a: BigInt, b: BigInt) = (a*b)/gcd(a,b)
-	final class XOREncryptedInputStream(in: java.io.InputStream, key: Array[Byte]) extends java.io.InputStream {
+	final class XOREncryptedInputStream(in: InputStream, key: Array[Byte]) extends InputStream {
 		private val rand = new SecureRandom(key.clone)
 		def getUnderlyingStream = in
 		def getKey = key
@@ -88,7 +91,7 @@ object Security {
 		rand.nextBytes(bytes)
 		bytes(0)
 	}
-	final class XOREncryptedOutputStream(out: java.io.OutputStream, key: Array[Byte]) extends java.io.OutputStream {
+	final class XOREncryptedOutputStream(out: OutputStream, key: Array[Byte]) extends OutputStream {
 		private val rand = new SecureRandom(key.clone)
 		def getUnderlyingStream = out
 		def getKey = key
@@ -144,15 +147,247 @@ object Security {
 		return s.toByteArray
 	}
 
-	//Generates a rsa keypair, the first pair of BigInts is the public key, the last BigInt is the private key.
-	/*def generateRSAKeypair: ((BigInt, BigInt), BigInt) = {
-		val rand = new SecureRandom
-		val p = generatePrime(1024)
-		val q = generatePrime(1024)
-		val n = p*q
-		val phiOfn = (p-1)*(q-1)
-		val e = new BigInt(BigInteger.valueOf(65537))
-		var d = new
-	}*/
+	class RSAInputStream(in: InputStream, prikey: RSAPrivateKey) extends InputStream {
+		private var buf = new Array[Byte](0)
+		private var offset = 0
+		//Recieves so there is atleast min items in the buffer
+		private def recieve(min: Int): Int = {
+			if (buf.length-offset-min < buf.length) {
+				val baos = new java.io.ByteArrayOutputStream
+				if (offset != buf.length) {
+					baos.write(buf, offset, buf.length-offset)
+				}
+				while (baos.size < min) {
+					val lbytes = new Array[Byte](4)
+					val lbr = in.read(lbytes)
+					if (lbr != 4) {//No more data
+						buf = baos.toByteArray
+						baos.close
+						offset = 0
+						return buf.length
+					}
+					val len = ((0xff & lbytes(0)) << 24) + ((0xff & lbytes(1)) << 16) + ((0xff & lbytes(2)) << 8) + (lbytes(3) & 0xff)
+					val chunkbytes = new Array[Byte](len)
+					val cbl = in.read(chunkbytes)
+					if (cbl != len)
+						throw new java.io.IOException("Unexpected EOF: In the middle of RSA block")
+					val rawbytes = prikey.decrypt(if (cbl == len) {chunkbytes} else {Arrays.copyOfRange(chunkbytes, 0, cbl)})
+					baos.write(rawbytes)
+				}
+				offset = 0
+				buf = baos.toByteArray
+				baos.close
+			}
+			return buf.length
+		}
+		override
+		def available: Int = {
+			val x = Math.max(buf.length - offset, 0)
+			if (x == 0) {
+				if (in.available > 0) {
+					return 1
+				}
+			}
+			return x
+		}
+		override
+		def close = in.close
+		override
+		def read: Int = {
+			recieve(1)
+			if (offset == buf.length)//No more data
+				return -1
+			val byte = buf(offset)
+			offset += 1
+			return byte
+		}
+		override
+		def read(b: Array[Byte]) = read(b, 0, b.length)
+		override
+		def read(b: Array[Byte], off: Int, len: Int): Int = {
+			val l = recieve(len)
+			println(l + " " + offset)
+			if (l == 0)
+				return -1
+			val target = Math.min(offset+len, l)
+			val canread = Math.max(target-offset,0)
+			for (i <- offset until target) {
+				b(i-offset+off) = buf(i)
+			}
+			offset = target
+			return canread
+		}
+		override
+		def skip(n: Long): Long = {
+			if (offset+n > buf.length) {
+				var c: Long = 0
+				while (c < n) {
+					val lbytes = new Array[Byte](4)
+					val lbr = in.read(lbytes)
+					if (lbr != 4) {//No more data
+						return c + lbr
+					}
+					val len = ((0xff & lbytes(0)) << 24) + ((0xff & lbytes(1)) << 16) + ((0xff & lbytes(2)) << 8) + (lbytes(3) & 0xff)
+					val chunkbytes = new Array[Byte](len)
+					val cbl = in.read(chunkbytes)
+					if (cbl != len)
+						throw new java.io.IOException("Unexpected EOF: In the middle of RSA block")
+					val rawbytes = prikey.decrypt(if (cbl == len) {chunkbytes} else {Arrays.copyOfRange(chunkbytes, 0, cbl)})
+					c += rawbytes.length
+					if (c >= n) {
+						if (c == n) {
+							buf = new Array(0)
+							offset = 0
+						} else {
+							buf = Arrays.copyOfRange(rawbytes, (rawbytes.length-c-n).intValue, rawbytes.length)
+						}
+					}
+				}
+			} else {
+				offset = (offset+n).intValue
+			}
+			return n
+		}
+	}
+	class RSAOutputStream(out: OutputStream, pubkey: RSAPublicKey) extends OutputStream {
+		private val buf = new Array[Byte](pubkey.getBlockSize)
+		private var offset = 0
+
+		def sendBuffer {
+			if (offset > 0) {
+				val encrypted = pubkey.encrypt(Arrays.copyOfRange(buf, 0, offset))
+				val l = encrypted.length
+				val lb = new Array[Byte](4)
+				lb(0) = (l >>> 24).byteValue
+				lb(1) = (l >>> 16).byteValue
+				lb(2) = (l >>> 8).byteValue
+				lb(3) = l.byteValue
+				out.write(lb)
+				out.write(encrypted)
+				offset = 0
+			}
+		}
+		override
+		def close {
+			sendBuffer
+			out.close
+		}
+		override
+		def flush {
+			sendBuffer
+			out.flush
+		}
+		override
+		def write(b: Int) {
+			if (offset >= pubkey.getBlockSize)
+				sendBuffer
+			buf(offset) = b.byteValue
+			offset += 1
+		}
+		override
+		def write(b: Array[Byte]) {
+			write(b, 0, b.length)
+		}
+		override
+		def write(b: Array[Byte], off: Int, len: Int) {
+			for (i <- off until len) {
+				buf(offset) = b(i)
+				offset += 1
+				if (offset >= pubkey.getBlockSize)
+					sendBuffer
+			}
+		}
+	}
+	object RSA {
+		def generateKeypair(bits: Int): (RSAPublicKey, RSAPrivateKey) = {
+			val gen = KeyPairGenerator.getInstance("RSA")
+			gen.initialize(bits)
+			val keypair = gen.generateKeyPair()
+			(new RSAPublicKey(keypair.getPublic.asInstanceOf[java.security.interfaces.RSAPublicKey]), new RSAPrivateKey(keypair.getPrivate.asInstanceOf[java.security.interfaces.RSAPrivateKey]))
+		}
+	}
+
+	class RSAPublicKey(private val pk: java.security.interfaces.RSAPublicKey) {
+		private val cipher = javax.crypto.Cipher.getInstance("RSA")
+		private var cipherinited = false
+
+		def encrypt(data: Array[Byte]) = {
+			if (data.length > getBlockSize)
+				throw new IllegalArgumentException("data longer than " + getBlockSize + " bytes")
+			if (!cipherinited) {
+				cipher.init(javax.crypto.Cipher.ENCRYPT_MODE, pk)
+				cipherinited = true
+			}
+			cipher.doFinal(data)
+		}
+		def getBlockSize = pk.getModulus.bitCount/4-11
+		def getBytes: Array[Byte] = pk.getEncoded
+		def writeTo(out: OutputStream) {
+			val pubkeybytes = getBytes
+			val pkbl = pubkeybytes.length
+			val pkblb = new Array[Byte](4)
+			pkblb(0) = (pkbl >>> 24).byteValue
+			pkblb(1) = (pkbl >>> 16).byteValue
+			pkblb(2) = (pkbl >>> 8).byteValue
+			pkblb(3) = pkbl.byteValue
+			out.write(pkblb)
+			out.write(pubkeybytes)
+		}
+	}
+	object RSAPublicKey {
+		def fromBytes(bytes: Array[Byte]) = {
+			val keyFactory = KeyFactory.getInstance("RSA")
+			val publicKeySpec = new java.security.spec.X509EncodedKeySpec(bytes)
+			new RSAPublicKey(keyFactory.generatePublic(publicKeySpec).asInstanceOf[java.security.interfaces.RSAPublicKey])
+		}
+		def readFrom(in: InputStream) = {
+			val lbytes = new Array[Byte](4)
+			in.read(lbytes)
+			val len = ((0xff & lbytes(0)) << 24) + ((0xff & lbytes(1)) << 16) + ((0xff & lbytes(2)) << 8) + (lbytes(3) & 0xff)
+			val bytes = new Array[Byte](len)
+			in.read(bytes)
+			fromBytes(bytes)
+		}
+	}
+	class RSAPrivateKey(private val pk: java.security.interfaces.RSAPrivateKey) {
+		private val cipher = javax.crypto.Cipher.getInstance("RSA")
+		private var cipherinited = false
+
+		def decrypt(data: Array[Byte]) = {
+			if (!cipherinited) {
+			cipher.init(javax.crypto.Cipher.DECRYPT_MODE, pk);
+				cipherinited = true
+			}
+			cipher.doFinal(data)
+		}
+		def getBlockSize = pk.getModulus.bitCount/4-11
+		def getBytes: Array[Byte] = pk.getEncoded
+		def writeTo(out: OutputStream) {
+			val prikeybytes = getBytes
+			val pkbl = prikeybytes.length
+			val pkblb = new Array[Byte](4)
+			pkblb(0) = (pkbl >>> 24).byteValue
+			pkblb(1) = (pkbl >>> 16).byteValue
+			pkblb(2) = (pkbl >>> 8).byteValue
+			pkblb(3) = pkbl.byteValue
+			out.write(pkblb)
+			out.write(prikeybytes)
+		}
+	}
+	object RSAPrivateKey {
+		def fromBytes(bytes: Array[Byte]) = {
+			val keyFactory = KeyFactory.getInstance("RSA")
+			val privateKeySpec = new java.security.spec.PKCS8EncodedKeySpec(bytes)
+			new RSAPrivateKey(keyFactory.generatePrivate(privateKeySpec).asInstanceOf[java.security.interfaces.RSAPrivateKey])
+		}
+		def readFrom(in: InputStream) = {
+			val lbytes = new Array[Byte](4)
+			in.read(lbytes)
+			val len = ((0xff & lbytes(0)) << 24) + ((0xff & lbytes(1)) << 16) + ((0xff & lbytes(2)) << 8) + (lbytes(3) & 0xff)
+			val bytes = new Array[Byte](len)
+			in.read(bytes)
+			fromBytes(bytes)
+		}
+	}
 
 }
