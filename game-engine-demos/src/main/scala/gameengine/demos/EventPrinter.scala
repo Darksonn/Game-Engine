803c024366267cc49df97cb79b796a284e19fb6a
Darksonn <kristoffer@ryhl.dk>
2013-08-13 18:52:28 +0200

    Stopped the eventprinter from spamming time pass events

--- game-engine-demos/src/main/scala/gameengine/demos/EventPrinter.scala
+++ game-engine-demos/src/main/scala/gameengine/demos/EventPrinter.scala
@@ -9,9 +9,22 @@ trait EventPrinter extends Game with EventInputStyle with ImperativeControlStyle
 	val height = 480
 	val title = "Event test!"
 
+	var tpEvents = 0l
+	var tp: BigInt = BigInt(0)
+
 	override def update(in: Input) = {
 		for (event <- in.queue) {
-			println(event)
+			if (event.isInstanceOf[TimePassEvent]) {
+				tp = tp + event.asInstanceOf[TimePassEvent].ns
+				tpEvents = tpEvents + 1
+			} else {
+				if (tpEvents > 0) {
+					println(tpEvents + " TimePassEvents totalling at " + tp + " nanoseconds")
+					tpEvents = 0
+					tp = BigInt(0)
+				}
+				println(event)
+			}
 		}
 		Seq()
 	}
